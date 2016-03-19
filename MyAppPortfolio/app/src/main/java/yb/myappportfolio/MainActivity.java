9ec0893a27724e567a7d93a1000444e0231aac6c
Yoann Bourgault <yoann.bourgault@gmail.com>
2016-03-19 01:23:16 +0100

    - onClick methods in xml - api 16+ compatibility

--- MyAppPortfolio/app/src/main/java/yb/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/yb/myappportfolio/MainActivity.java
@@ -1,9 +1,8 @@
 package yb.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,67 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initButtons();
     }
 
-    private void initButtons() {
-        final Button
-                spotify = (Button) findViewById(R.id.button_spotify_app),
-                scores = (Button) findViewById(R.id.button_scores_app),
-                library = (Button) findViewById(R.id.button_library_app),
-                bigger = (Button) findViewById(R.id.button_built_it_bigger_app),
-                capstone = (Button) findViewById(R.id.button_capstone);
-        initSpotify(spotify);
-        initScores(scores);
-        initLibrary(library);
-        initBigger(bigger);
-        initCapstone(capstone);
+    public void initSpotify(View spotify) {
+        displayWorkInProgress();
     }
 
-    private void initSpotify(Button spotify) {
-        spotify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayWorkInProgress();
-            }
-        });
+    public void initScores(View scores) {
+        displayWorkInProgress();
     }
 
-
-    private void initScores(Button scores) {
-        scores.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayWorkInProgress();
-            }
-        });
+    public void initLibrary(View library) {
+        displayWorkInProgress();
     }
 
-    private void initLibrary(Button library) {
-        library.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayWorkInProgress();
-            }
-        });
+    public void initBigger(View bigger) {
+        displayWorkInProgress();
     }
 
-    private void initBigger(Button bigger) {
-        bigger.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayWorkInProgress();
-            }
-        });
+    public void initReader(View reader) {
+        displayWorkInProgress();
     }
 
-    private void initCapstone(Button capstone) {
-        capstone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayWorkInProgress();
-            }
-        });
+    public void initCapstone(View capstone) {
+        displayWorkInProgress();
     }
 
     private void displayWorkInProgress() {
